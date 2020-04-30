c094ffae32550b1eef9118e351d7846fc21220ee
Ionut <predaionut2001@gmail.com>
2020-04-30 19:31:28 +0300

    Error messages

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,9 @@ ActiveRecord::Schema.define(version: 2020_04_28_124149) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
     t.datetime "update_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
