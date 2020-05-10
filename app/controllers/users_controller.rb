d89544db4d5dfe3a776b867bc281e013c6797463
Ionut <predaionut2001@gmail.com>
2020-05-10 12:09:04 +0300

    Restrict actions from controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :updte]
+  before_action :require_same_user, only: [:edit, :update]
 
     def show
-      @user = User.find(params[:id])
       @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -14,12 +16,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-      @user = User.find(params[:id])
     end
 
     def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
+        if @user.update(user_params)
         flash[:notice] = "Your account information was succesfully updated"
         redirect_to @user
       else
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the Alpha-Blog #{@user.username}, you have succesfully signed up"
         redirect_to articles_path
       else
@@ -40,6 +41,17 @@ class UsersController < ApplicationController
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
     end
 
 end
